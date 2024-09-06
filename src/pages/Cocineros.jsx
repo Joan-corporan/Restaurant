9b66189ea3ec9a90074ef708dd6b158861050099
nicovidal <nico.vidal.m@gmail.com>
2024-09-06 11:53:07 -0400

    comanda ok

--- src/pages/Cocineros.jsx
+++ src/pages/Cocineros.jsx
@@ -2,7 +2,7 @@ import { CardPedido } from "../components/CardPedido";
 
 export const Cocinero = () => {
   return (
-    <div className="d-flex flex-wrap justify-content-start">
+    <div className="d-flex flex-wrap justify-content-start" style={{backgroundColor:'grey'}}>
       <CardPedido />
       <CardPedido />
       <CardPedido />

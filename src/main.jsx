d3f9071fd3eca2dd2624d8ee862f19736afc2374
nicovidal <nico.vidal.m@gmail.com>
2024-09-10 11:22:18 -0300

    fds

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { Restaurant } from './Restaurant.jsx'
 
 createRoot(document.getElementById('root')).render(
